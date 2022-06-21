8455cd05107793ee982ce9c91b49e5bc547e1c2e
Alec Torres <atorresla24@gmail.com>
2022-06-21 02:44:18 -0400

    lab06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: [],
+      lat: '',
+      lon: ''
+    };
+  }
+
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_ALEC_TORRES_API_KEY}&q=${this.state.city}&format=json`;
+    let cityInfo = await axios.get(url);
+    this.setState={
+      city: cityInfo.data[0].lon
+    };
+    console.log(cityInfo.data[0].lon);
+  }
+
+  handleInput = (event) => {
+    this.setState({
+     city: event.target.value 
+    });
+  }
+
+  render() {
+    //let coordinates = this.state.city.map((city) => {return <li>{city.boundingbox}</li>});
+    return(
+      <>
+      <h1>City Explorer</h1>
+      <form onSubmit={this.handleSubmit}>
+        <label>Choose City </label>
+        <input type='text' onInput={this.handleInput}></input>
+        <button type='submit'>Explore!</button>
+      </form>
+      <ul>
+        {}
+      </ul>
+      </>
+    )
+  };
 }
+
+//function App() {
+    //return (
+      <div className="App">
+        <header className="App-header">
+          City Explorer
+        </header>
+        <main>
+          <body>
+          <form>
+          <label for='city'>Choose City</label>
+          <input type='text' id='cityChosen' name='cityChosen'></input>
+          <button></button>
+          </form>
+          </body>
+        </main>
+      </div>
+    //);
+  //}
+
 
 export default App;
