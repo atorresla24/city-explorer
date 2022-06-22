4365ce70d8c6cad0fbf2b742dd130507879cdc74
Alec Torres <atorresla24@gmail.com>
2022-06-22 19:22:27 -0400

    lab 06

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import './App.css';
 import React from 'react'
 import axios from 'axios';
+import Image from 'react-bootstrap/Image';
+import Alert from 'react-bootstrap/Alert';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,9 +14,9 @@ class App extends React.Component {
       cityData: {},
       lat: '',
       lon: '',
-      map: '',
       error: false,
-      errorMessage: ''
+      errorMessage: '',
+      map: ''
     };
   }
 
@@ -22,17 +25,16 @@ class App extends React.Component {
     try {
     event.preventDefault();
     let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_ALEC_TORRES_API_KEY}&q=${this.state.city}&format=json`;
-    let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_ALEC_TORRES_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=12`;
     let cityInfo = await axios.get(url);
-    let cityMap = await axios.get(mapUrl);
+    let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_ALEC_TORRES_API_KEY}&center=${cityInfo.data[0].lat},${cityInfo.data[0].lon}&zoom=12`;
     console.log(cityInfo);
     this.setState({
       cityData: cityInfo.data[0],
       city: cityInfo.data[0].display_name,
       lat: cityInfo.data[0].lat,
       lon: cityInfo.data[0].lon,
-      map: cityMap,
-      error: false
+      error: false,
+      map: mapUrl
     });
     //console.log(cityInfo.data[0].lat);
   } catch(error) {
@@ -53,7 +55,8 @@ class App extends React.Component {
     //console.log(this.state);
     //img src = https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_ALEC_TORRES_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=12
     return(
-      <>
+      <html>
+      
       <h1>City Explorer</h1>
       <form onSubmit={this.handleSubmit}>
         <label>Choose City </label>
@@ -63,15 +66,11 @@ class App extends React.Component {
       <p>City: {this.state.city}</p>
       <p>Latitude: {this.state.lat}</p>
       <p>Longitude: {this.state.lon}</p>
-      <img src={this.state.map} alt={this.state.city}/>
-      {this.state.error
-      ? <p>{this.state.errorMessage}</p>
-      :
-      <ul>
-        {}
-      </ul>
+      <Image src={this.state.map} alt={this.city}/>
+      {this.state.error &&
+       <Alert variant={'danger'}>{this.state.errorMessage}</Alert>
   }
-      </>
+      </html>
     )
   };
 }
